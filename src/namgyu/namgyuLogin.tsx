d73d2ac6cdd29385469e6e07763a651082205f3c
tph7897 <tph7897@naver.com>
2023-08-01 10:02:42 +0900

    test

---
+++ src/namgyu/namgyuLogin.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const namgyuLogin = () => {
+  return (
+    <div>namgyuLogin</div>
+  )
+}
---
+++ src/namgyu/namgyuSignup.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const namgyuSignup = () => {
+  return (
+    <div>namgyuSignup</div>
+  )
+}
