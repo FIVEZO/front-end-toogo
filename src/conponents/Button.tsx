f53c62c157649bae6a54891941a89aec67c8262f
Baek-Taejun <126327414+Baek-Taejun@users.noreply.github.com>
2023-08-02 23:26:30 +0900

    feat & 버튼 컨포넌트 분리

--- src/conponents/Button.tsx
+++ src/conponents/Button.tsx
@@ -1,13 +1,81 @@
-import React from 'react'
+import React from 'react';
+import { css, styled } from 'styled-components';
+
+type ButtonProps = {
+  backgroundColor?: string;
+  fontWeight?: string;
+  onClick?: () => void;
+  type?: "primary" | "medium" | "small";
+  color?: string; 
+  children?: string; 
+  fontSize?: string; 
+};
 
 
-
-
-function Button(){
+const Button: React.FC<ButtonProps> = ({
+  backgroundColor = "#CFCED7",
+  fontWeight,
+  onClick,
+  type,
+  color, 
+  children,
+  fontSize, 
+}) => {
   return (
-    <div></div>
-  )
-}
+    <StyledButton
+      backgroundColor={backgroundColor}
+      fontWeight={fontWeight}
+      onClick={onClick}
+      type={type}
+      color={color} 
+      fontSize={fontSize}
+    >
+{children} 
+    </StyledButton>
+  );
+};
 
-export default Button
+export default Button;
 
+const StyledButton = styled.div<ButtonProps>`
+  font-family: Pretendard;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.1;
+  letter-spacing: normal;
+  text-align: center;
+  width: 384px;
+  height: 46px;
+  border-radius: 8.53px;
+  display: inline-flex;
+  align-items: center;
+  margin-bottom: 16px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  color: ${({ color }) => color}; 
+  font-weight: ${({ fontWeight }) => fontWeight};
+  font-size: ${({ fontSize }) => fontSize};
+
+  ${({ type }) =>
+    type === "primary"
+      ? css`
+          width: 384px;
+          height: 46px;
+        `
+      : type === "medium"
+      ? css`
+          width: 180px;
+          height: 46px;
+        `
+      : type === "small"
+      ? css`
+          width: 72px;
+          height: 36px;
+        `
+      : css``}; 
+
+`;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-
 import Button from '../conponents/Button'
+import { styled } from 'styled-components'
+
 
 
 
@@ -15,7 +16,12 @@ export const Main = () => {
     <h1>Main</h1>
     <div>Main</div>
     <button>버튼입니다.</button>
+    <Button type='primary'>Primary 버튼</Button>
+
+    <Button type='medium'color="green">Medium 버튼</Button>
+    <Button type='small' fontSize="10px">Small 버튼</Button>
 
     </div>
   )
 }
+
