444bdcea9edadbdf47d61aacb60b52e3339a44f8
Sangjun-L <pilot03067@naver.com>
2023-08-11 22:51:01 +0900

    feat & Footer 완성

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -7,6 +7,7 @@ import { Cards } from '../conponents/Cards';
 import { cardItem } from '../types/posts';
 import Header from '../conponents/Header';
 import { useNavigate } from 'react-router-dom';
+import Footer from '../conponents/Footer';
 
 
 export const Main: React.FC = () => {
@@ -40,6 +41,7 @@ export const Main: React.FC = () => {
           <Cards key={item.id} items={item} />
         ))}
       </StCardContainer>
+      <Footer/>
     </div>
   )
 }
