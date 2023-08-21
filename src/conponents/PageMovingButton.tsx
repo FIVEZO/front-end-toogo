7cebe08e3989d53384e35071b534aa86e533951e
Sangjun-L <pilot03067@naver.com>
2023-08-21 23:34:45 +0900

    feat & 페이지네이션 프론트 완성, 백에서 totalpage받아와서 적용하면 끝

--- src/conponents/PageMovingButton.tsx
+++ src/conponents/PageMovingButton.tsx
@@ -1,29 +1,46 @@
-import React from 'react';
-import styled from 'styled-components';
-import "../fonts/Font.css";
+// PageMovingButton.tsx
+
+import React from "react";
+import styled from "styled-components";
 
 interface ButtonProps {
   onClick: () => void;
   text: string;
+  color: string;
+  backgroundColor: string;
 }
 
-const PageMovingButton: React.FC<ButtonProps> = ({ onClick, text }) => {
-  return <Button onClick={onClick}>{text}</Button>;
+const PageMovingButton: React.FC<ButtonProps> = ({
+  onClick,
+  text,
+  color,
+  backgroundColor,
+}) => {
+  return (
+    <Button onClick={onClick} color={color} backgroundColor={backgroundColor}>
+      {text}
+    </Button>
+  );
 };
 
 export default PageMovingButton;
 
-const Button = styled.button`
-  width: 323px;
-  height: 56px;
-  padding: 20px 40px;
-  background-color: white;
-  color: #484848;
-  font-family: 'Pretendard';
+interface ButtonStyleProps {
+  color: string;
+  backgroundColor: string;
+}
+
+const Button = styled.button<ButtonStyleProps>`
+  width: 40px;
+  height: 40px;
+  padding: 8px;
+  background-color: ${(props) => props.backgroundColor};
+  color: ${(props) => props.color};
+  font-family: "Pretendard";
   font-size: 16px;
   font-style: normal;
-  font-weight: 400;
-  border-radius: 88px;
-  border: 1px solid #9A9A9A;
+  font-weight: 700;
+  border-radius: 12px;
+  border: none;
   justify-content: center;
 `;
